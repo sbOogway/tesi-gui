9888919aef61e9d4f162e5106a74bff217227e08
Juice Man <92176188+sbOogway@users.noreply.github.com>
2025-12-16 17:59:53 +0100

    updating include path v5

--- user_cross_compile_setup.cmake
+++ user_cross_compile_setup.cmake
@@ -18,7 +18,7 @@ set(CMAKE_CXX_COMPILER $ENV{TOOLS}g++)
 
 
 set(COMMON_CONTROL_LIBRARIES $ENV{STAGE}/usr/lib/libcommon-control.a)
-set(COMMON_CONTROL_INCLUDE_DIRS $ENV{STAGE}/usr/include/common-control.h)
+set(COMMON_CONTROL_INCLUDE_DIRS $ENV{STAGE}/usr/include/common-control/)
 
 set(EVDEV_INCLUDE_DIRS $ENV{STAGE}/usr/include/libevdev-1.0/)
 set(EVDEV_LIBRARIES $ENV{STAGE}/usr/lib/libevdev.so)
